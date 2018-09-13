11102eb9f95111799f6dab43ebe6fefb8dea27cc
julianajlk <julianajlk@gmail.com>
2018-09-13 12:20:14 -0400

    fixed edit toggle function - moved edit patch fetch to Controller. Need to clean up code

--- when_what_wear/clothes_frontend/src_oojs/index.js
+++ when_what_wear/clothes_frontend/src_oojs/index.js
@@ -1,6 +1,9 @@
+//make controller global so that it can be called inside Outfit.js
+const controller = new Controller()
+
 document.addEventListener('DOMContentLoaded', function() {
-  let controller = new Controller()
-  document.querySelector('.add_outfit_button').addEventListener('click', controller.toggleForm.bind(controller))
+  document.querySelector('.add-outfit-button').addEventListener('click', controller.toggleAddForm.bind(controller))
   controller.fetchOutfits()
+
 
 })
